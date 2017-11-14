974df8d450a16a4c730e65ee73f6ae0719e09afd
Abhinish Raj <31284774+Abhinish-prodevans@users.noreply.github.com>
2017-11-14 14:47:39 +0530

    new way of promise

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -10,20 +10,15 @@ var geocodeAddress = (address, callback) => {
         , (error,response,body) => {
 
             if(error){
-                console.log('Program is unable to connect to google servers');
+                callback('Program is unable to connect to google servers');
             }else if (body.status ==='ZERO_RESULTS'){
-                console.log('Invalid Url kindly enter a valid Address');
+                callback('Invalid Url kindly enter a valid Address');
             }else if (body.status === 'OK'){
                 callback(undefined, {
                     address : body.results[0].formatted_address,
                     latitude : body.results[0].geometry.location.lat,
                     longitude : body.results[0].geometry.location.lng
                 })
-
-                /*/!*console.log(JSON.stringify(body,undefined,2));*!/
-                console.log(`Address: ${}`);
-                console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-                console.log(`Longitude : ${body.results[0].geometry.location.lng}`);*/
             }
         });
 
